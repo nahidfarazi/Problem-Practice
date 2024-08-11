a949f4ef5df67e9b3e165642840449f370864490
bsr <bsr@bsr.bsr>
2024-08-11 01:26:34 -0500

    maximum in array done

--- maximum_number_in_array.c
+++ maximum_number_in_array.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
 int main() {
+int myArray[5],n;
+printf("enter your array size : ");
+scanf("%d",&n);
+ 
+for (int i = 0; i < n; i++)
+{
+    printf("enter your array item : ");
+    scanf("%d",&myArray[i]);
+}
 
+int max;
+max = myArray[0];
+for (int i = 1; i <5; i++)
+{
+    if(max<myArray[i]) max = myArray[i];
+    
+}
+printf("Maximum: %d\n",max); 
  
  return 0;
 }
